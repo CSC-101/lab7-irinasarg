6e7fc09429e1526b940a2c8555a7cd8a9b000bd5
irinasarg <isargsya@calpoly.edu>
2024-11-11 22:06:13 -0800

    submit

--- keyboard.py
+++ keyboard.py
@@ -4,14 +4,14 @@ import convert
 def gather_numbers() -> list[float]:
     numbers = []
     while True:
-        user_input = input("Please enter a number")
+        user_input = input("Please enter a number: ")
         if user_input.lower() == "done":
             break
         number = convert.str_to_float(user_input)
         if number is not None:
             numbers.append(number)
-    return numbers
-print("Sum of numbers = , sum(numbers)")
+
+    print("Sum of numbers = ", sum(numbers))
 
 if __name__ == "__main__":
         numbers = gather_numbers()
